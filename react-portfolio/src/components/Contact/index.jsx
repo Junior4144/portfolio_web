0c7247ad034e50abee2b9fd896f9a8ff7a10066f
Junior4144 <94208651+Junior4144@users.noreply.github.com>
2025-01-07 19:44:31 -0600

    Feat: added styling

--- react-portfolio/src/components/Contact/index.jsx
+++ react-portfolio/src/components/Contact/index.jsx
@@ -55,7 +55,7 @@ const Contact = () => {
                         <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
-                                    <input type="text" name="name" placeholder='name' required />
+                                    <input type="text" name="name" placeholder='Name' required />
                                 </li>
                                 <li className='half'>
                                     <input type="email" name="email" placeholder='Email' required />
